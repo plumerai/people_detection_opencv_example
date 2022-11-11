e3933c69e88a737b2766fe055c5fdf7d891fcce0
Cedric Nugteren <web@cedricnugteren.nl>
2022-11-11 14:13:15 +0100

    Add OpenCV C++ demo, testing, and docs (#1)
    * Set the copyright notice in the license
    
    * Add OpenCV example source code
    
    * Add test infrastructure
    
    * Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# people_detection_opencv_example
-Example people-detection library usage in a simple OpenCV webcam-to-display pipeline
+# Plumerai People Detection OpenCV pipeline example
+
+This is the source code for a simple version of using the Plumerai People Detection library in an end-to-end demo setting. It runs on Linux, uses OpenCV and V4L, and retrieves input data from a webcam and writes box coordinates to console and to an output video stream displayed on screen. It can be modified as needed for proto-typing with the Plumerai People Detection library.
+
+The Plumerai People Detection library itself is not included: this repository can't be used without access to the library. If you do not have access to the library and would like to evaluate it, then contact us at [plumerai.com/contact_us](https://plumerai.com/contact_us). For more information, see [plumerai.com/people-detection](https://plumerai.com/people-detection).
+
+## Linux system requirements
+
+The example application uses [OpenCV](https://opencv.org/). Using the `CAP_V4L` OpenCV API, it connects to the webcam with [V4L (video 4 linux)](https://en.wikipedia.org/wiki/Video4Linux). Most (USB) webcams compatible with V4L should work with this example demo application, as long as they support the Motion-JPEG (MJPEG) input compression format. If you have V4L installed on your system (see below), this can be verified by running `v4l2-ctl --list-formats-ext`.
+
+On a Debian-based system (e.g. Ubuntu) the pre-requisites (OpenCV with V4L) can be installed as follows:
+```bash
+apt install libopencv-dev libopencv-highgui-dev libopencv-videoio-dev libv4l-dev
+```
+In case of issues or for other Linux systems, see the [official OpenCV release page](https://opencv.org/releases/).
+
+## Configuring the camera settings
+
+First, make sure the camera settings (`camera_height`, `camera_width`, and `camera_id`) are set correctly near the top of the `src/opencv_example.cc` file.
+
+The higher the input resolution, the better the results can become. However, note that this might slow down the entire example application, because the camera capture and video-displaying might take up more resources. The framerate reported is purely for the Plumerai People Detection algorithm itself, and does not count camera capture or displaying of the results.
+
+Note that the example application itself (the camera capture / video decoding and displaying) is not optimized for speed. Furthermore, the people detection algorithm in its current form is not optimized for speed on x86 systems, only for targets such as Arm Cortex-A.
+
+## Compiling the example application
+
+The OpenCV example can be compiled with a C++11 compiler linking to both OpenCV4 and the Plumerai People Detection library, e.g.:
+```bash
+g++ -std=c++11 -O3 src/opencv_example.cc -I/path/to/plumeraipeopledetection/include/ /path/to/plumeraipeopledetection/lib/<PLATFORM>/libplumeraipeopledetection.a `pkg-config --cflags --libs opencv4` -ldl -pthread -o opencv_example
+```
+Here, `<PLATFORM>` above refers to the platform you are on, e.g. `x86_64` or `aarch64`. In case of an older OpenCV version, remove the `4` in the `opencv4` part above.
+
+The example app can then be executed without any command-line arguments, e.g.:
+```
+./opencv_example
+```
---
+++ src/opencv_example.cc
@@ -0,0 +1,137 @@
+// Copyright (C) 2022 Plumerai Ltd under the Apache 2.0 license.
+//
+// This code implements a simple demo around the Plumerai People Detection
+// library using OpenCV to capture webcam input and display to screen. You can
+// modify the code as needed for your prototyping use-case, it might not work
+// out-of-the-box depending on your system and camera used. See the README for
+// more information.
+
+// Standard C++ library includes
+#include <chrono>
+#include <string>
+#include <vector>
+
+// Regular OpenCV libraries, these should be installed on your system
+#include "opencv2/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/videoio.hpp"
+
+// The Plumerai People Detection library. If you do not have access to this
+// library and would like to evaluate it, then contact us via
+// https://plumerai.com/contact_us. For more information, see our website
+// https://plumerai.com/people-detection, and docs.plumerai.com for information
+// how to use it.
+#include "plumerai/people_detection.h"
+
+// Input camera settings (change as needed)
+constexpr auto width = 1280;
+constexpr auto height = 720;
+constexpr auto camera_id = 0;  // 0 == the first camera, 1 == the second, etc.
+
+// The following line sets the capture API to Video4Linux. Change this as needed
+// on your system. See the OpenCV website for a list of all options:
+// https://docs.opencv.org/4.6.0/d4/d15/group__videoio__flags__base.html
+constexpr auto camera_capture_api = cv::CAP_V4L;
+
+// The following line sets the camera format to Motion-JPEG. If your camera does
+// not support this, then it could be changed to another format.
+const auto camera_capture_format = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+
+// These are settings to draw bounding boxes on screen. They do not need to be
+// modified. A bounding-box will use any of the following 19 colours. If there
+// are more than 19 boxes found, it will re-use the same colour.
+constexpr int num_colours = 19;
+const int colours[num_colours][3] = {
+    {230, 25, 75},  {60, 180, 75},   {255, 225, 25}, {0, 130, 200},
+    {245, 130, 48}, {145, 30, 180},  {70, 240, 240}, {240, 50, 230},
+    {210, 245, 60}, {250, 190, 212}, {0, 128, 128},  {220, 190, 255},
+    {170, 110, 40}, {255, 250, 200}, {128, 0, 0},    {170, 255, 195},
+    {128, 128, 0},  {255, 215, 180}, {0, 0, 128}};
+
+int main() {
+  // Set-up the Plumerai People Detection algorithm
+  auto ppd = plumerai::PeopleDetection(height, width);
+
+  // Set-up the output window
+  const auto window_text = "Plumerai People Detection";
+  cv::namedWindow(window_text, cv::WINDOW_AUTOSIZE);
+
+  // Set-up the camera, see the above constants for the settings
+  cv::VideoCapture camera(camera_id, camera_capture_api);
+  camera.set(cv::CAP_PROP_FRAME_WIDTH, width);
+  camera.set(cv::CAP_PROP_FRAME_HEIGHT, height);
+  camera.set(cv::CAP_PROP_FOURCC, camera_capture_format);
+  if (!camera.isOpened()) {
+    // If this is printed the camera might not be attached or it might not
+    // support the settings set above (width & height, capture API or format)
+    printf("Error: Could not open camera %d, verify the settings\n", camera_id);
+    return -1;
+  }
+
+  // Run the demo in an infinite loop
+  cv::Mat cv_image;
+  while (true) {
+    // Retrieve an image from the camera
+    if (camera.read(cv_image) == false) {
+      printf("Error: could not read frame.\n");
+      return -1;
+    }
+
+    // Process the frame using the Plumerai People Detection library. This
+    // function is timed and framerate in FPS is reported. Note that the
+    // Plumerai People Detection library is not optimized for all systems,
+    // see the README for more details.
+    auto start_time = std::chrono::steady_clock::now();
+    auto predictions = ppd.process_frame(cv_image.data);
+    auto elapsed_time = std::chrono::steady_clock::now() - start_time;
+
+    // Report the time and framerate the above function took
+    auto time_ms =
+        std::chrono::duration<double, std::milli>(elapsed_time).count();
+    auto fps = static_cast<int>(1000 / time_ms);
+    printf("Processing took %.1f ms (%d fps)\n", time_ms, fps);
+
+    // Process the resulting bounding-boxes if there are any
+    for (auto &p : predictions) {
+      auto x_min =
+          std::max(0, std::min(static_cast<int>(p.x_min * width), width - 1));
+      auto y_min =
+          std::max(0, std::min(static_cast<int>(p.y_min * height), height - 1));
+      auto x_max =
+          std::max(0, std::min(static_cast<int>(p.x_max * width), width - 1));
+      auto y_max =
+          std::max(0, std::min(static_cast<int>(p.y_max * height), height - 1));
+
+      // Write the resulting coordinates to stdout
+      printf(
+          "Found box #%d with confidence %.2f at (x=%d, y=%d) - (x=%d, y=%d)\n",
+          p.id, p.confidence, x_min, y_min, x_max, y_max);
+
+      // Also draw the bounding-boxes on the video
+      auto colour = colours[p.id % num_colours];
+      for (int c = 0; c < 3; ++c) {
+        for (int x = x_min; x < x_max; x++) {
+          cv_image.data[(y_min * width + x) * 3 + c] = colour[c];
+          cv_image.data[(y_max * width + x) * 3 + c] = colour[c];
+        }
+        for (int y = y_min; y < y_max; y++) {
+          cv_image.data[(y * width + x_min) * 3 + c] = colour[c];
+          cv_image.data[(y * width + x_max) * 3 + c] = colour[c];
+        }
+      }
+
+      // Any user-processing code can be added here, e.g. writing the box
+      // coordinates into a file, sending them over the network, or modifying
+      // the image (e.g. person blurring for privacy).
+    }
+
+    // Finally display the webcam image with the resulting bounding-boxes
+    cv::imshow(window_text, cv_image);
+    const auto wait_ms = 30;  // can be changed to force a certain framerate
+    if ((cv::waitKey(wait_ms) & 0xFF) == 27) {
+      // Exit when ESC is pressed
+      break;
+    }
+  }
+  return 0;
+}
